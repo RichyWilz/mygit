7fdbcd558481cfa71f1dbc191f8b28a9d3f2dda7
Kayiira Richard <richardwilsonevarist@gmail.com>
2022-06-23 17:23:34 +0300

    Tried to create an app called books and initiated its model as well as making migrations. Its however not yet finished

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+ #import datetime
 
 # Create your models here.
+class Book(models.Model):
+    title            = models.CharField(max_length=120)
+    publication_date = models.DateField()
+    subject_area     = models.CharField(max_length=40)
+    author           = models.CharField(max_length=80)
+     #availability     = models.ForeignKey
+    description      = models.TextField()
+    cover_photo      = models.FilePathField(path="/images")
+
