ae1edb6322f62cabdead04641b4fa7216f03766c
MKBenjie <mugaggabenjie@gmail.com>
2022-07-04 08:14:03 +0300

    modified urls and added app_name

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'books'
 urlpatterns = [
     path("",views.book_overview,name="book_overview"),
     path("<int:pk>/",views.book_detail,name="book_detail"),
+    # path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from books.models import Book
+from django.views import generic
+from django.urls import reverse_lazy
+from .forms import SignUpForm
+
 # Create your views here.
 def book_overview(request):
     books = Book.objects.all().order_by('-publication_date')
@@ -14,3 +18,8 @@ def book_detail(request, pk):
         'book': book
     }
     return render(request,'book_detail.html', context)
+
+# class SignUpView(generic.CreateView):
+#     form_class = SignUpForm
+#     # success_url = reverse_lazy('login')
+#     template_name = 'registration/signup.html'
